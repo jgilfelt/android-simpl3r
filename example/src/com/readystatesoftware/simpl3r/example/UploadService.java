c4308f73cb61932eb2475ba9f27714287b5ee5fb
Jeff Gilfelt <jgilfelt@gmail.com>
2012-11-30 15:42:51 +0000

    added license, readme

--- example/src/com/readystatesoftware/simpl3r/example/UploadService.java
+++ example/src/com/readystatesoftware/simpl3r/example/UploadService.java
@@ -1,3 +1,18 @@
+/***
+ * Copyright (c) 2012 readyState Software Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
 package com.readystatesoftware.simpl3r.example;
 
 import java.io.File;
@@ -138,12 +153,12 @@ public class UploadService extends IntentService {
 	
 	private String md5(String s) {
 		try {
-			// Create MD5 Hash
+			// create MD5 Hash
 			MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
 			digest.update(s.getBytes());
 			byte messageDigest[] = digest.digest();
 
-			// Create Hex String
+			// create Hex String
 			StringBuffer hexString = new StringBuffer();
 			for (int i=0; i<messageDigest.length; i++)
 				hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
